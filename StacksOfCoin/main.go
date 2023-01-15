faca811284f0163fc096be679061ac9b5b44541a
KolesnikovOleg <OKolesnikov1812@gmail.com>
2023-01-15 20:57:24 +0300

    stackdOfCoin removed duplication

--- StacksOfCoin/main.go
+++ StacksOfCoin/main.go
@@ -6,41 +6,44 @@ func Solution(A []int) int {
 	leftPointer := 0
 	rightPointer := len(A) - 1
 	leftCoastSum, rightCoastSum := 0, 0
+	leftMoveCoinsCount, rightMoveCoinsCount := 0, 0
+
+	doLeftMove := func() {
+		leftCoastSum += leftMoveCoinsCount
+		A[leftPointer] -= leftMoveCoinsCount + leftMoveCoinsCount //not optional
+		A[leftPointer+1] += leftMoveCoinsCount
+		leftPointer += 1
+	}
+
+	doRightMove := func() {
+		rightCoastSum += rightMoveCoinsCount
+		A[rightPointer] -= rightMoveCoinsCount + rightMoveCoinsCount //not optional
+		A[rightPointer-1] += rightMoveCoinsCount
+		rightPointer -= 1
+	}
 
 	for leftPointer != rightPointer {
-		leftMoveCoinsCount := int((A[leftPointer] - A[leftPointer]%2) / 2)
-		rightMoveCoinsCount := int((A[rightPointer] - A[rightPointer]%2) / 2)
+		leftMoveCoinsCount = int((A[leftPointer] - A[leftPointer]%2) / 2)
+		rightMoveCoinsCount = int((A[rightPointer] - A[rightPointer]%2) / 2)
 		possibleLeftCoast := leftCoastSum + leftMoveCoinsCount
 		possibleRightCoast := rightCoastSum + rightMoveCoinsCount
 
 		if possibleRightCoast > possibleLeftCoast {
-			leftCoastSum = possibleLeftCoast
-			A[leftPointer] -= leftMoveCoinsCount + leftMoveCoinsCount //not optional
-			A[leftPointer+1] += leftMoveCoinsCount
-			leftPointer += 1
+			doLeftMove()
 			continue
 		}
 		if possibleRightCoast < possibleLeftCoast {
-			rightCoastSum = possibleRightCoast
-			A[rightPointer] -= rightMoveCoinsCount + rightMoveCoinsCount //not optional
-			A[rightPointer-1] += rightMoveCoinsCount
-			rightPointer -= 1
+			doRightMove()
 			continue
 		}
 		// if equal
 		//check next
 		if (A[leftPointer+1] + leftMoveCoinsCount) >= (A[rightPointer-1])+rightMoveCoinsCount {
-			leftCoastSum += leftMoveCoinsCount
-			A[leftPointer] -= leftMoveCoinsCount + leftMoveCoinsCount //not optional
-			A[leftPointer+1] += leftMoveCoinsCount
-			leftPointer += 1
+			doLeftMove()
 			continue
 		}
 
-		rightCoastSum += rightMoveCoinsCount
-		A[rightPointer] -= rightMoveCoinsCount + rightMoveCoinsCount //not optional
-		A[rightPointer-1] += rightMoveCoinsCount
-		rightPointer -= 1
+		doRightMove()
 		continue
 
 	}
@@ -50,6 +53,6 @@ func Solution(A []int) int {
 
 func main() {
 	// Заменить на нужжый массив для проверки
-	A := []int{1, 0, 1, 1}
+	A := []int{2, 3, 1, 3}
 	fmt.Println(Solution(A))
 }
